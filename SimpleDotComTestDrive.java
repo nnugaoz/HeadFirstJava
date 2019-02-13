05bcf3e782256bbc819cfd61ea9f52ec2712188c
home_gaoz <nnugaozhi@hotmail.com>
2019-02-13 23:57:38 +0800

    战舰游戏 修改bug 统一位置击中三次也能击沉战舰

--- SimpleDotComTestDrive.java
+++ SimpleDotComTestDrive.java
@@ -4,6 +4,26 @@ public class SimpleDotComTestDrive{
 		
 		GameHelper helper = new GameHelper();
 		
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		
+		int randomNum = (int)(Math.random() * 5);
+		
+		int[][] locations = {{randomNum,0},{randomNum+1,0},{randomNum+2,0}};
+		
+		theDotCom.setLocationCells(locations);
+		
+		boolean isAlive = true;
+		
+		while(isAlive){
+			String guess = helper.getUserInput("enter a number");
+			String result = theDotCom.checkYourself(guess);
+			numOfGuesses++;
+			
+			if(result.equals("kill")){
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guess.");
+			}
+		}
 		
 	}
 }
