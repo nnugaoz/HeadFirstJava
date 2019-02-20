13132dc151803e9c6643044575ff1d3428c17e48
gaoz@home <nnugaozhi@hotmail.com>
2019-02-21 00:04:02 +0800

    增加‘数字与静态’章节 示例代码

---
+++ FooTestDriver.java
@@ -0,0 +1,20 @@
+public class FooTestDriver{
+	public static void main(String[] args){
+		//Foo f = new Foo();
+		//f.go();
+		
+		//Foo2 f2 = new Foo2();
+		//f2.go();
+		
+		//Foo3 f3 = new Foo3();
+		//f3.go();
+		
+		//Foo4 f4 = new Foo4();
+		//f4.go();
+		
+		//Foo5 f5 = new Foo5();
+		//f5.go(1234);
+		
+		Foo6.go(5678);
+	}
+}
